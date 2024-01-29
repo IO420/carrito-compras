3eacf631c705603bf84795b58a20a363cfeec4a7
IO <IO@IO.com>
2024-01-29 04:25:05 -0600

    casi no hice nada

--- Component/Item/Item.tsx
+++ Component/Item/Item.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
 import indra from '@/imagenes/indra_01.jpg'
+import faber1 from '@/imagenes/faber-castell-black-edition-50.png'
 
 export interface item{
     id:number;
@@ -15,19 +16,22 @@ export default function Item() {
   return (
     <div className="container d-flex "style={{background:"#E3E0DD"}}>
 
+        <div>
         <Image
         //src={`@/${imgid}.jpg`}
-        src={indra}
-        width={250}
-        height={150}
+        src={faber1}
+        width={200}
+        height={200}
         alt="img"
         />
 
+        </div>
 
         <h5>titulo</h5>
         <p>descripcion</p>
         <h6>costo</h6>
-        <button type="button">Agregar</button>
+        <button type="button">ver</button>
+        <button type="button">Comprar</button>
     </div>
   )
 }
